6016b831976896155e5b2383fa5bbe1d1a330327
Eseteselase <esete.s.berhanu@gmail.com>
2024-07-31 00:21:52 +0300

    r

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module 1-app
+Set up basic Flask application
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class that represent available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index() -> str:
+    """Display Hello World message
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
